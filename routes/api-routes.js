f47cb39f9e5ebf55f7cc67cda874824cd37afd2c
Joe Maniaci <joe.maniaci1@gmail.com>
2021-02-23 16:04:40 -0500

    added morgan logger

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,47 +4,46 @@ const db = require("../models");
 module.exports = function (app) {
   app.get("/api/workouts", (req, res) => {
     db.Workout.find({})
-      .then(dbWorkouts => {
+      .then((dbWorkouts) => {
         res.json(dbWorkouts);
       })
-      .catch(err => {
+      .catch((err) => {
         res.status(400).json(err);
       });
   });
 
   app.put("/api/workouts/:id", (req, res) => {
-    console.log(req.params.id);
-    console.log(req.body);
+    
     db.Workout.findByIdAndUpdate(req.params.id,
       {
         $push: { exercises: req.body },
       },
       { new: true},
     )
-      .then(workouts => {
+      .then((workouts) => {
         res.json(workouts);
       })
-      .catch(err => {
+      .catch((err) => {
         res.status(400).json(err);
       });
   });
-
-app.post("/api/workouts/", (req, res) => {
-  db.Workout.create(req.body)
-  .then(workouts => {
-    res.json(workouts);
-  })
-  .catch(err => {
-    res.status(400).json(err);
+  
+  app.post("/api/workouts/", (req, res) => {
+    db.Workout.create(req.body)
+    .then((workouts) => {
+      res.json(workouts);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
   });
-});
 
 app.get("/api/workouts/range", (req, res) => {
   db.Workout.find({})
-      .then(dbWorkouts => {
+      .then((dbWorkouts) => {
         res.json(dbWorkouts);
       })
-      .catch(err => {
+      .catch((err) => {
         res.status(400).json(err);
       });
   });
